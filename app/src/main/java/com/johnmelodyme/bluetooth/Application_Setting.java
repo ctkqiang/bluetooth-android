99e7445d96653a8bfe08718c73eee56f0ca0ae5e
John Melody Me <johnmelodyme@icloud.com>
2020-01-14 13:13:06 +0800

    Application Setting updated

--- app/src/main/java/com/johnmelodyme/bluetooth/Application_Setting.java
+++ app/src/main/java/com/johnmelodyme/bluetooth/Application_Setting.java
@@ -1,10 +1,23 @@
 package com.johnmelodyme.bluetooth;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.List;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 /**
  * @AUTHOR : JOHN MELODY ME
@@ -14,9 +27,9 @@ import java.util.List;
  */
 
 public class Application_Setting extends AppCompatActivity {
-    private String CLASSIC_SETTING[] = {""};
     private String OTHER_SETTING[] = {"About", "Source"};
     ListView Other_Setting, Preference;
+    ArrayAdapter O_setting, C_setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,32 @@ public class Application_Setting extends AppCompatActivity {
         setContentView(R.layout.activity_application__setting);
 
         Other_Setting = (ListView) findViewById(R.id.other);
-        Preference = (ListView) findViewById(R.id.classic);
+
+        Context context = Application_Setting.this;
+        O_setting = new ArrayAdapter<String>(context, R.layout.support_simple_spinner_dropdown_item, OTHER_SETTING);
+        Other_Setting.setAdapter(O_setting);
+
+        Other_Setting.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String ItemClicked;
+                ItemClicked = (String) parent.getItemAtPosition(position);
+
+                if (ItemClicked.equals("About")){
+                    Log.w("Tag", "ABOUT");
+                } else if (ItemClicked.equals("Source")){
+                    SweetAlertDialog pDialog = new SweetAlertDialog(Application_Setting.this, SweetAlertDialog.PROGRESS_TYPE);
+                    pDialog.getProgressHelper().setBarColor(Color.parseColor("#A5DC86"));
+                    pDialog.setTitleText("Loading to the source........");
+                    pDialog.setCancelable(false);
+                    pDialog.show();
+                    Intent SOURCE;
+                    SOURCE = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://github.com/johnmelodyme/bluetooth-android"));
+                    startActivity(SOURCE);
+                    pDialog.dismissWithAnimation();
+                }
+            }
+        });
     }
 }
